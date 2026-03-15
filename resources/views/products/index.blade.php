2cf0d33f5eb347908bfa413405674116383bbb33
FameNan <nannapat_chaip@cmu.ac.th>
2026-03-16 01:15:06 +0700

    change to use old script

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop - Our Products</title>
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop - Our Products</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50 dark:bg-gray-900">
 
