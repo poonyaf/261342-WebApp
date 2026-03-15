014c0be192537ac2a3c893e6f0c98d65b8ee438e
FameNan <nannapat_chaip@cmu.ac.th>
2026-03-16 00:13:23 +0700

    fix trust proxies headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //for not show form not secure
-        $middleware->trustProxies(at:'*');
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+    );
         $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ]);
